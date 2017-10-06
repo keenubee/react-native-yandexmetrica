17c171b4f7b405d39838a7012e1b9b356bc7b554
Aleksey <aleks.dev@gmail.com>
2017-10-06 11:01:00 +0300

    Removed deprecated @Override

--- android/src/main/java/com/reactnativeyandexmetrica/YandexMetricaPackage.java
+++ android/src/main/java/com/reactnativeyandexmetrica/YandexMetricaPackage.java
@@ -25,7 +25,6 @@ public class YandexMetricaPackage implements ReactPackage {
         return Arrays.<NativeModule>asList(new YandexMetricaModule(reactContext));
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
